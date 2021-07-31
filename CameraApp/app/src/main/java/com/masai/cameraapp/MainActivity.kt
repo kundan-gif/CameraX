d3f2b32eac1832138e9260b758cf61f4b1dd9e3f
Kundan Kumar <kundan211097@gmail.com>
2021-07-31 14:47:11 +0530

    Camera UI and CameraX dependecies

---
+++ CameraApp/app/src/main/java/com/masai/cameraapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.masai.cameraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.camera_ui)
+    }
+}
